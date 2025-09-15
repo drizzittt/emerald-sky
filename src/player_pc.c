25c31b765db8debbcd5fe6b670357ef107cc3d4f
rihi52 <rileyhire7@gmail.com>
2025-09-14 22:12:23 -0400

    added alden back to devon 3f. misc other changes to allow porymap to work. added rare candies and max repels to starting box.

--- src/player_pc.c
+++ src/player_pc.c
@@ -225,6 +225,8 @@ static const struct MenuAction sItemStorage_MenuActions[] =
 static const u16 sNewGamePCItems[][2] =
 {
     { ITEM_POTION, 1 },
+    {ITEM_RARE_CANDY, 200},
+    {ITEM_MAX_REPEL, 200},
     { ITEM_NONE, 0 }
 };
 
