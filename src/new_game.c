84519480fd325ccb035ff610f5c98497b7555ec2
rihi52 <rileyhire7@gmail.com>
2025-09-22 06:17:05 -0400

    minor fixes after merge

--- src/new_game.c
+++ src/new_game.c
@@ -130,7 +130,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), MAP_NUM(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F),-1, -1, -1);
+    SetWarpDestination(MAP_GROUP(MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), MAP_NUM(MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_2F),-1, -1, -1);
     WarpIntoMap();
 }
 
